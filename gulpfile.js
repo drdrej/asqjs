a305efb7be18d19a43607a5b397e98421694bd47
drdrej <ask@touchableheroes.com>
2017-01-14 20:19:52 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+
+
+
+gulp.task( 'default', function() {
+    // place code for your default task here
+    console.log( "> hello world gulp. " );
+
+    var git = require('gulp-git');
+
+    // gulp.task('add', function(){
+    return gulp.src('.')
+            .pipe(git.add({args: '--all'}))
+            .pipe(git.commit('initial commit', {args: '-a'}) )
+            .pipe(git.push('origin', 'master',
+                    function (err) {
+                        if (err) throw err;
+                    }));
+        ;
+    // });
+});
